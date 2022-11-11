51b8edda28e05ea908b947d5153744705e9f22b9
tatt61880 <tatt61880@gmail.com>
2022-11-11 23:07:49 +0900

    .

--- src/colors.js
+++ src/colors.js
@@ -8,16 +8,16 @@
   }
 
   const colors = {};
-  colors[showkoban.states.none] = {fill: 'white', stroke: '#aaa', text: '#ccc'};
-  colors[showkoban.states.wall] = {fill: '#222', stroke: '#666', text: 'white'};
+  colors[showkoban.states.none] = {fill: '#ffffff', stroke: '#aaaaaa', text: '#888888'};
+  colors[showkoban.states.wall] = {fill: '#222222', stroke: '#666666', text: '#bbbbbb'};
   for (let i = showkoban.states.targetMin; i <= showkoban.states.targetMax; ++i) {
-    colors[i] = {fill: 'pink', stroke: 'red', text: 'black'};
+    colors[i] = {fill: 'pink', stroke: 'red', text: '#993333'};
   }
   for (let i = showkoban.states.otherMin; i <= showkoban.states.otherMax; ++i) {
-    colors[i] = {fill: '#e5e5e5', stroke: '#aaa', text: 'black'};
+    colors[i] = {fill: '#e5e5e5', stroke: '#aaa', text: '#555555'};
   }
   for (let i = showkoban.states.userMin; i <= showkoban.states.userMax; ++i) {
-    colors[i] = {fill: 'aqua', stroke: 'blue', text: 'black'};
+    colors[i] = {fill: 'aqua', stroke: 'blue', text: '#333399'};
   }
 
   colors.line = '#888';
