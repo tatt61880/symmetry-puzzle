cd4324d3ef321ec142711fbe1218bac19433491b
tatt61880 <tatt61880@gmail.com>
2022-11-13 12:13:00 +0900

    Update test.js

--- src/test.js
+++ src/test.js
@@ -6,11 +6,52 @@
   showkoban.levels = require('./levels.js');
   showkoban.Level = require('./class-level.js');
 
-  /*
-  for (const levelObj of showkoban.levels) {
+  const dys = [-1, 0, 1, 0];
+  const dxs = [0, 1, 0, -1];
+
+  for (const idx in showkoban.levels) {
+    const levelObj = showkoban.levels[idx];
+    const levelId = Number(idx) + 1;
+    const res = testLevel(levelObj);
+    if (!res) {
+      console.error(`Error: Level-${levelId} Test failed.`);
+      process.exitCode = 1;
+      return;
+    }
+  }
+
+  process.exitCode = 0;
+  return;
+
+  function testLevel(levelObj) {
     const level = showkoban.Level();
     level.applyObj(levelObj, true);
-    console.log(level);
+
+    const r = level.getLevelObj()?.r;
+    if (r === null) return false;
+    for (const dirChar of r) {
+      const dir = Number(dirChar);
+      const dx = dxs[dir];
+      const dy = dys[dir];
+      const moveFlag = level.updateMoveFlags(dx, dy);
+      if (!moveFlag) {
+        console.error('Error: moveFlag failed.');
+        return false;
+      }
+      const center = level.getRotateCenter(showkoban.states.isTarget);
+      const clearFlag = center !== null;
+      if (clearFlag) {
+        console.error('Error: Cleared on the way.');
+        return false;
+      }
+      level.move();
+    }
+    const center = level.getRotateCenter(showkoban.states.isTarget);
+    const clearFlag = center !== null;
+    if (!clearFlag) {
+      console.error('Error: clearFlag failed.');
+      return false;
+    }
+    return true;
   }
-  */
 })();
