3d18d4f69503a418fe4925784dba897c19412fd6
tatt61880 <tatt61880@gmail.com>
2022-12-04 09:33:54 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -699,64 +699,66 @@
       g.appendChild(rect);
     }
 
-    const bestStep = level.getBestStep();
-    // クリア時のメッセージ
-    if (clearFlag) {
-      const text = app.svg.createText(blockSize, {x: level.getWidth() * 0.5, y: level.getHeight() - 1, text: 'CLEAR', fill: 'blue'});
-      text.setAttribute('font-size', `${blockSize * 0.8}px`);
-      text.setAttribute('font-weight', 'bold');
-      g.appendChild(text);
-      const clearStep = undoInfo.getIndex();
-      {
-        const levelObj = level.getLevelObj();
-        const w = levelObj.w;
-        const h = levelObj.h;
-        const s = levelObj.s;
-        const r = levelObj.r;
-        const replayStr = undoInfo.getReplayStr();
-        if (bestStep !== undefined) {
-          savedata.saveSteps(levelObj, replayStr);
-        }
-        const levelObjStr = `{w: ${w}, h: ${h}, s: '${s}', r: '${replayStr}', ja: '${levelObj.ja}'},`;
-        console.log(levelObjStr);
-        if (r === undefined) {
-          console.warn('参考用公式記録の情報がありません！');
-        } else if (clearStep < bestStep) {
-          console.log(`参考用公式記録を破りました！\n参考用公式記録[${bestStep}] → あなたの記録[${clearStep}] (${clearStep - bestStep} 手)`);
-        } else {
-          console.log(`参考用公式記録は ${bestStep} 手です。\n(差: ${clearStep - bestStep} 手)`);
-          if (replayStr === r) {
-            console.log('(完全に同じ手順です。)');
+    if (!editMode) {
+      const bestStep = level.getBestStep();
+      // クリア時のメッセージ
+      if (clearFlag) {
+        const text = app.svg.createText(blockSize, {x: level.getWidth() * 0.5, y: level.getHeight() - 1, text: 'CLEAR', fill: 'blue'});
+        text.setAttribute('font-size', `${blockSize * 0.8}px`);
+        text.setAttribute('font-weight', 'bold');
+        g.appendChild(text);
+        const clearStep = undoInfo.getIndex();
+        {
+          const levelObj = level.getLevelObj();
+          const w = levelObj.w;
+          const h = levelObj.h;
+          const s = levelObj.s;
+          const r = levelObj.r;
+          const replayStr = undoInfo.getReplayStr();
+          if (bestStep !== undefined) {
+            savedata.saveSteps(levelObj, replayStr);
+          }
+          const levelObjStr = `{w: ${w}, h: ${h}, s: '${s}', r: '${replayStr}', ja: '${levelObj.ja}'},`;
+          console.log(levelObjStr);
+          if (r === undefined) {
+            console.warn('参考用公式記録の情報がありません！');
+          } else if (clearStep < bestStep) {
+            console.log(`参考用公式記録を破りました！\n参考用公式記録[${bestStep}] → あなたの記録[${clearStep}] (${clearStep - bestStep} 手)`);
+          } else {
+            console.log(`参考用公式記録は ${bestStep} 手です。\n(差: ${clearStep - bestStep} 手)`);
+            if (replayStr === r) {
+              console.log('(完全に同じ手順です。)');
+            }
           }
         }
-      }
 
-      {
-        const color = getStepColor(clearStep, bestStep);
-        const text = app.svg.createText(blockSize, {x: level.getWidth() * 0.5, y: 0, text: `${clearStep} steps`, fill: color});
-        text.setAttribute('font-size', `${blockSize * 0.7}px`);
+        {
+          const color = getStepColor(clearStep, bestStep);
+          const text = app.svg.createText(blockSize, {x: level.getWidth() * 0.5, y: 0, text: `${clearStep} steps`, fill: color});
+          text.setAttribute('font-size', `${blockSize * 0.7}px`);
+          text.setAttribute('font-weight', 'bold');
+          g.appendChild(text);
+        }
+        if (settings.autoMode) {
+          nextLevelTimerId = setTimeout(gotoNextLevel, AUTO_NEXT_LEVEL_DELAY);
+        }
+      } else {
+        const text = app.svg.createText(blockSize, {x: level.getWidth() * 0.5, y: level.getHeight() - 1, text: `${undoInfo.getIndex()} steps`, fill: 'white'});
         text.setAttribute('font-weight', 'bold');
         g.appendChild(text);
       }
-      if (settings.autoMode) {
-        nextLevelTimerId = setTimeout(gotoNextLevel, AUTO_NEXT_LEVEL_DELAY);
-      }
-    } else {
-      const text = app.svg.createText(blockSize, {x: level.getWidth() * 0.5, y: level.getHeight() - 1, text: `${undoInfo.getIndex()} steps`, fill: 'white'});
-      text.setAttribute('font-weight', 'bold');
-      g.appendChild(text);
-    }
 
-    // 自己最高記録
-    if (levelId !== null && !clearFlag) {
-      const levelObj = level.getLevelObj();
-      const highestScore = savedata.getHighestScore(levelObj);
-      if (highestScore !== null) {
-        const color = getStepColor(highestScore, bestStep);
-        const text = app.svg.createText(blockSize, {x: level.getWidth() * 0.5, y: 0, text: `${highestScore}`, fill: color});
-        text.setAttribute('font-size', `${blockSize * 0.7}px`);
-        text.setAttribute('font-weight', 'bold');
-        g.appendChild(text);
+      // 自己最高記録
+      if (levelId !== null && !clearFlag) {
+        const levelObj = level.getLevelObj();
+        const highestScore = savedata.getHighestScore(levelObj);
+        if (highestScore !== null) {
+          const color = getStepColor(highestScore, bestStep);
+          const text = app.svg.createText(blockSize, {x: level.getWidth() * 0.5, y: 0, text: `${highestScore}`, fill: color});
+          text.setAttribute('font-size', `${blockSize * 0.7}px`);
+          text.setAttribute('font-weight', 'bold');
+          g.appendChild(text);
+        }
       }
     }
 
