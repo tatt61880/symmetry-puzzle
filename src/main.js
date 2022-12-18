15248fe781651f41c6165c9ee9ed0a7fad2402b2
tatt61880 <tatt61880@gmail.com>
2022-12-19 01:11:56 +0900

    .

--- src/main.js
+++ src/main.js
@@ -708,7 +708,8 @@
           if (bestStep !== undefined) {
             savedata.saveSteps(levelObj, replayStr);
           }
-          if (!settings.autoMode) {
+
+          {
             const levelParams = `w: ${w}, h: ${h}, s: '${s}', r: '${replayStr}', step: ${replayStr.length}` + (levelObj.subject !== undefined ? `, subject: '${levelObj.subject}'` : '');
             const levelObjStr = `{${levelParams}},`;
             consoleLog(levelObjStr);
