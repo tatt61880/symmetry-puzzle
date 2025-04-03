0cb5922cebd2b7e0640c4458e21b20ed3afd2153
tatt61880 <tatt61880@gmail.com>
2025-04-03 23:46:29 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -844,6 +844,7 @@
     updateEditAxisButton();
     updateEditMode(editMode);
     if (!editMode) {
+      // 現在の盤面を初期値とするレベルに更新しておかないと、クリア時のセーブ情報がバグります。
       common.level = new app.Level({ levelObj: common.level.getCurrentLevelObj(), checkMode: common.checkMode });
       const base = location.href.split('?')[0];
       const urlQuery = common.level.getCurrentUrlQuery();
