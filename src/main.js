a632ec879cb59f96f43ed034ef30d6e628249d66
tatt61880 <tatt61880@gmail.com>
2026-03-21 21:09:33 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1895,7 +1895,6 @@
         // 動けないときは盤面を振動させます。
         addAnimationClass(elems.main.svg, 'animation-illegal-move');
         sound.playBump();
-        navigator.vibrate?.(30);
       } else {
         const className = (() => {
           switch (common.level.getAxisType()) {
@@ -1924,7 +1923,6 @@
         elems.main.svg.style.setProperty('--animation-origin-illegal', `${(blockSize * cx) / 2}px ${(blockSize * cy) / 2}px`);
         addAnimationClass(elems.main.svg, className);
         sound.playBump();
-        navigator.vibrate?.(30);
       }
     }
   }
